48b408298afb286bc1dde91b951b88803086cca7
Chioma Princess Ohwo <ohwoprincess@gmail.com>
2025-10-15 18:05:06 +0100

    Refine error handling for MySQL connection
    Updated error handling to use mysql.connector.Error for better clarity.

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,7 +5,6 @@ If the database already exists, the script will not fail.
 """
 
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
@@ -21,7 +20,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
