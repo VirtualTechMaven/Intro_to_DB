dbb2b6798732c01beb3448a195f16384e6159a5c
Chioma Princess Ohwo <ohwoprincess@gmail.com>
2025-10-15 18:14:07 +0100

    Fix case sensitivity in books table query

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,5 @@
--- Use the alx_book_store database
 USE alx_book_store;
 
--- Display full description of the 'books' table without using DESCRIBE or EXPLAIN
 SELECT 
     COLUMN_NAME,
     COLUMN_TYPE,
@@ -10,5 +8,5 @@ SELECT
     COLUMN_DEFAULT,
     EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'Books'
+WHERE TABLE_NAME = 'books'
 AND TABLE_SCHEMA = 'alx_book_store';
